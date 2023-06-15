55948832f2a662c4317d18fe04d01878393d4fe3
Kevin Minutti <minutti70@gmail.com>
2023-06-15 00:06:10 -0400

    Add example: instance ver class

--- python-oop/main.py
+++ python-oop/main.py
@@ -5,17 +5,15 @@ class ExampleClass:
     def __method(self):
         print(self.__value)
 
+class SampleClass:
+    class_attr = 100
 
-def run_example_class():
-    ex = ExampleClass("Hello!")
-    print(vars(ex))
+    def __init__(self, instance_attr):
+        self.instance_attr = instance_attr
 
-    # print(ex.__value) <- throws an error
-    #           ^^^^^^^^^^
-    # AttributeError: 'ExampleClass' object has no attribute '__value'
-
-    print(ex._ExampleClass__value)
-    ex._ExampleClass__method()
+    def method(self):
+        print(f"Class attribute: {self.class_attr}")
+        print(f"Instance attribute: {self.instance_attr}")
 
 class ObjectCounterC:
     num_instances = 0
@@ -28,6 +26,25 @@ class ObjectCounter:
     def __init__(self):
         self.num_instances+=1
 
+class SampleClass:
+    class_attr = 100
+    def __init__(self, instance_attr):
+        self.instance_attr = instance_attr
+    def method(self):
+        print(f"Class attr: {self.class_attr}")
+        print(f"Instance attr: {self.instance_attr}")
+
+def run_example_class():
+    ex = ExampleClass("Hello!")
+    print(vars(ex))
+
+    # print(ex.__value) <- throws an error
+    #           ^^^^^^^^^^
+    # AttributeError: 'ExampleClass' object has no attribute '__value'
+
+    print(ex._ExampleClass__value)
+    ex._ExampleClass__method()
+
 def run_object_counter():
     for _ in range(4):
         ObjectCounterC()
@@ -38,4 +55,8 @@ def run_object_counter():
     assert ex.num_instances == 1
     print(ex.num_instances)
 
-run_object_counter()
+def run_sample_class():
+    print(SampleClass.class_attr)
+    print(SampleClass(300).instance_attr)
+
+run_sample_class()
